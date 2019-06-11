69593ba7fc500735e52aca91a635cc453c528d42
Jittapan Pluemsumran <secret@jittapan.app>
2019-06-11 09:45:10 +0700

    Updates

---
+++ precache-manifest.4f97773fe0b09882d9cd82de7d7e830b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98cbc8277e14aa49c646243937630fc",
+    "url": "/attendance-generator/index.html"
+  },
+  {
+    "revision": "62a67b4df989bf072fbc",
+    "url": "/attendance-generator/static/css/2.f4948e68.chunk.css"
+  },
+  {
+    "revision": "b08195a7d2faf7e5de54",
+    "url": "/attendance-generator/static/css/main.654b0622.chunk.css"
+  },
+  {
+    "revision": "62a67b4df989bf072fbc",
+    "url": "/attendance-generator/static/js/2.75c7c23e.chunk.js"
+  },
+  {
+    "revision": "b08195a7d2faf7e5de54",
+    "url": "/attendance-generator/static/js/main.47626c1a.chunk.js"
+  },
+  {
+    "revision": "21c3e2cc9e1adbc0d62e",
+    "url": "/attendance-generator/static/js/runtime~main.fa590435.js"
+  }
+]);
