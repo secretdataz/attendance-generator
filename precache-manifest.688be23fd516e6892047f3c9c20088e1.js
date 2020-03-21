da662ba880f882a7a764793025e7acb8c5182bc9
secretdataz <secretdataz@users.noreply.github.com>
2020-03-21 09:41:17 +0000

    Automated deployment: Sat Mar 21 09:41:17 UTC 2020 3106caa2c969c2f1476fcd189d2c0a96d236711b

---
+++ precache-manifest.688be23fd516e6892047f3c9c20088e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825d208ad178a161dc75b25e3db7ce71",
+    "url": "/attendance-generator/index.html"
+  },
+  {
+    "revision": "8348764b4ee08266727d",
+    "url": "/attendance-generator/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "343073bf0b91ded6d79e",
+    "url": "/attendance-generator/static/js/2.697172dd.chunk.js"
+  },
+  {
+    "revision": "8348764b4ee08266727d",
+    "url": "/attendance-generator/static/js/main.e4d3c523.chunk.js"
+  },
+  {
+    "revision": "5e9da3927ce5ea21776e",
+    "url": "/attendance-generator/static/js/runtime-main.aac1b741.js"
+  }
+]);
